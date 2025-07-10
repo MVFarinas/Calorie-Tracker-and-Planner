f75c3574512727e315c37e36d23d1869b380a602
Mark <farinas@ualberta.ca>
2025-07-10 16:15:36 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,18 @@
 #This program is a modification of the original source code to utilize DSAs 
-# This version will utiilize Linked Lists, allow for moving averages, data validation, and file persistence 
+#This version will utiilize Linked Lists, allow for moving averages, data validation, and file persistence
 
+class DailyEntry:
+    def __init__(self):
+        {}
+
+class CaloriesLog:
+    def __init__(self):
+        {}
+
+class MaintenanceEstimator:
+    def __init__(self):
+        {}
+
+class GoalPlanner:
+    def __init__(self):
+        {}
