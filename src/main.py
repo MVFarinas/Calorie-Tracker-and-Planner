be9fc7150d93f265e57a2a1c55c7aa4dcf50931c
Mark <farinas@ualberta.ca>
2025-07-19 11:31:51 -0600

    Updated main.py
    updates to DailyEntry class and CaloriesLog class

--- src/main.py
+++ src/main.py
@@ -2,11 +2,14 @@
 #This version will utiilize Linked Lists, allow for moving averages, data validation, and file persistence
 
 class DailyEntry:
-    def __init__(self):
-        {}
+    def __init__(self, date, weight, calories):
+        self._date = date
+        self._weight = weight
+        self._calories = calories
 
 class CaloriesLog:
     def __init__(self):
+        self._entries = [] #initiate empty list
         {}
 
 class MaintenanceEstimator:
