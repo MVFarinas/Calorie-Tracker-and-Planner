1735ac64e4591cb1bb056f588081ab9d52d86a43
Mark <farinas@ualberta.ca>
2025-08-06 19:01:25 -0600

    update main.py
    remove wildcard imports

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 #This program is a modification of the original source code to utilize DSAs 
 #This version will utiilize Linked Lists, allow for moving averages, data validation, and file persistence
 
-from scipy.optimize import*
-from datetime import*
+from scipy.optimize import minimize, minimize_scalar
+from datetime import datetime
 
 class DailyEntry:
     def __init__(self, date, weight, calories):
