fd23f56c6e74d39c3e717eefcf2c0fb0eba2e4df
Mark <farinas@ualberta.ca>
2025-08-09 22:48:19 -0600

    update entry.py
    format changes

--- src/entry.py
+++ src/entry.py
@@ -1,6 +1,3 @@
-#This program is a modification of the original source code to utilize DSAs 
-#This version will utiilize Linked Lists, allow for moving averages, data validation, and file persistence
-
 from scipy.optimize import minimize, minimize_scalar
 from datetime import datetime, timedelta
 
