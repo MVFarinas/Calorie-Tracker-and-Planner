7cf58cd889e1487832dcbb296cae9ac4b08081f7
Mark <farinas@ualberta.ca>
2025-08-14 23:08:05 -0600

    update entry.py
    formatting

--- src/entry.py
+++ src/entry.py
@@ -107,7 +107,7 @@ class GoalPlanner:
         
         return self._maintenance + daily_calories
 
-    #To do:
+    # To do:
     # 1. Utilize scipy.optimize in GoalPlanner instead of simple calculations 
     # 2. Add error handling and logging 
     # 3. Write unit tests for each class and method
