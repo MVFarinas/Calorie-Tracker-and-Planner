9c25c1b6c7ba0cb9ae76e98e5e0fcce59f12ca93
Mark <farinas@ualberta.ca>
2025-08-21 23:39:57 -0600

    update example.py

--- src/example.py
+++ src/example.py
@@ -10,17 +10,27 @@ if __name__ == "__main__":
 
     # Example: add entries, but validate each one before inserting
     raw_entries = [
-    DailyEntry(datetime(2025, 7, 1), weight=200, calories=2200),
-    DailyEntry(datetime(2025, 7, 2), weight=198.8, calories=2200),
-    DailyEntry(datetime(2025, 7, 3), weight=198.1, calories=2200),
-    DailyEntry(datetime(2025, 7, 4), weight=197.7, calories=2200),
-    DailyEntry(datetime(2025, 7, 5), weight=197.5, calories=2200),
-    DailyEntry(datetime(2025, 7, 6), weight=197.4, calories=2200),
-    DailyEntry(datetime(2025, 7, 7), weight=197.3, calories=2200),
-    DailyEntry(datetime(2025, 7, 8), weight=197.2, calories=2200),
-    DailyEntry(datetime(2025, 7, 9), weight=197.2, calories=2200),
-    DailyEntry(datetime(2025, 7, 10), weight=197.2, calories=2200),
+        DailyEntry(datetime(2025, 7, 1), weight=200, calories=2200),
+        DailyEntry(datetime(2025, 7, 2), weight=198.8, calories=2200),
+        DailyEntry(datetime(2025, 7, 3), weight=198.2, calories=2150),
+        DailyEntry(datetime(2025, 7, 4), weight=197.9, calories=2100),
+        DailyEntry(datetime(2025, 7, 5), weight=197.7, calories=2100),
+        DailyEntry(datetime(2025, 7, 6), weight=197.6, calories=2050),
+        DailyEntry(datetime(2025, 7, 7), weight=197.5, calories=2050),
+        DailyEntry(datetime(2025, 7, 8), weight=197.5, calories=2050),
+        DailyEntry(datetime(2025, 7, 9), weight=197.4, calories=2000),
+        DailyEntry(datetime(2025, 7, 10), weight=197.4, calories=2000),
 
+        DailyEntry(datetime(2025, 7, 11), weight=197.1, calories=2200),
+        DailyEntry(datetime(2025, 7, 12), weight=197.0, calories=2200),
+        DailyEntry(datetime(2025, 7, 13), weight=197.0, calories=2200),
+        DailyEntry(datetime(2025, 7, 14), weight=197.0, calories=2200),
+        DailyEntry(datetime(2025, 7, 15), weight=196.9, calories=2200),
+        DailyEntry(datetime(2025, 7, 16), weight=196.9, calories=2200),
+        DailyEntry(datetime(2025, 7, 17), weight=196.8, calories=2200),
+        DailyEntry(datetime(2025, 7, 18), weight=196.8, calories=2200),
+        DailyEntry(datetime(2025, 7, 19), weight=196.8, calories=2200),
+        DailyEntry(datetime(2025, 7, 20), weight=196.8, calories=2200),
     ]
     for entry in raw_entries:
         if EntryValidator.is_valid(entry):
