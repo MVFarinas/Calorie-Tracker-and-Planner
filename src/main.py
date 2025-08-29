857b69d13c7f6a0c35767810ee0ed3617fd191d3
Mark <farinas@ualberta.ca>
2025-08-28 23:13:39 -0600

    update main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,15 @@
 # main program logic
 
-from util.file_loader import load_file
+from util.file_loader import FileLoader
 from entry import CaloriesLog, DailyEntry, MaintenanceCalculator
 
 if __name__ == "__main__":
     log = CaloriesLog()
 
     # Swap between different file formats for testing
-    entries = load_file("data/sample_data.csv")
-    #entries = load_file("data/sample_data.json")
-    #entries = load_file("data/sample_data.txt")
+    entries = FileLoader.load_file("data/sample_data.csv")
+    #entries = FileLoader.load_file("data/sample_data.json")
+    #entries = FileLoader.load_file("data/sample_data.txt")
 
     for entry in entries:
         log.add_entry(entry)
